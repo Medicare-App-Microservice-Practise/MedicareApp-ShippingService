88eac45887ecff24ada4962ab97760926c53a096
arvend <arvendrajan100@gmail.com>
2024-03-04 15:54:58 +0800

    Initial Commit

---
+++ src/main/java/com/medicare/shippingservice/MedicareServiceShippingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medicare.shippingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareServiceShippingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareServiceShippingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicare/shippingservice/MedicareServiceShippingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicare.shippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareServiceShippingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
